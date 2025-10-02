da47ab4c72311bad3b6170483800c32afb32873c
Woolliscroft <arunwoolliscroft@icloud.com>
2025-10-02 15:31:54 +0100

    Update renderer.c

--- renderer.c
+++ renderer.c
@@ -1,8 +1,13 @@
-#indef VK_RENDERER_H
-#define VK_RENDERER_H
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
 
-typedef struct VkState VkState; // pass shader data
-void vk_begin(VKState* state);
-void vk_renderRect(VKState*, float x, float y, float w, float h);
-void vk_end(VkState* state);
-#endif
+#include "renderer.h"
+
+struct VkState{
+  unsigned int shaderProgram;
+};
+
+void vk_begin(VKState* state){}
+void vk_renderRect(VKState*, float x, float y, float w, float h){}
+void vk_end(VkState* state) {}
